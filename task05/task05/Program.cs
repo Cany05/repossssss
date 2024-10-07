f289861786374077360ca0c15e6188286e148e88
Cany05 <canyrei866@gmail.com>
2024-10-07 17:31:41 +0500

    Задание выполнено

---
+++ task05/task05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x =Math.Round( MathF(1, 2) + MathF(2, 2) + MathF(3, 5) + MathF(4, 3),3);
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+        static double MathF(double a, double b)
+        {
+            return ((a + Math.Pow(Math.E, -a)) / b);
+        }
+    }
+}
