8354b01cc4f08a5b898aa1fc3e98114141338afe
Cany05 <canyrei866@gmail.com>
2024-10-21 16:52:28 +0500

    задание 7-1

---
+++ task07-1/task07-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = GetNum("k");
+            var m = GetNum("m");
+            var n = GetNum("n");
+
+            if (CheckTriangle(k, m, n))
+                Console.WriteLine("Утверждение истинно");
+            else 
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+        }
+
+
+        static int GetNum(string number)
+        {
+            Console.WriteLine("Введите число "+number);
+             return int.Parse(Console.ReadLine());
+        }
+        static bool CheckTriangle(int k, int m, int n)
+        {
+            return (k+m)>n && (m+n)>k && (k+n)>m;
+              
+        }
+    }
+}
---
+++ task07-1/task07-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task07-1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task07-1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a6f728d6-33ed-42d1-86c5-e2a5d798fdcb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
