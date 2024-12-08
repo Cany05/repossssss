fd894ca6c55cba7fd4df79d78f8c422e3e9040af
Cany05 <canyrei866@gmail.com>
2024-12-08 14:00:15 +0500

    task 10_6 complete

---
+++ task10_6/task10_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cattleAmount, money, bulls, cows, calves;
+            double totalCost;
+            money = 100;
+            cattleAmount = 100;
+
+            for (bulls = 0; bulls <= money / 10; bulls++)
+            {
+                for (cows = 0; cows <= (money - bulls * 10) / 5; cows++)
+                {
+
+                    calves = cattleAmount - bulls - cows;
+                    if (calves >= 0)
+                    {
+                        totalCost = bulls * 10 + cows * 5 + calves * 0.5;
+                        if (totalCost == 100)
+                        {
+                            Console.WriteLine($"Быков: {bulls}; Коров: {cows}; Телят: {calves}");
+
+                        }
+                    }
+                }
+
+               
+            }
+                Console.ReadKey();
+
+        }
+    }
+}
---
+++ task10_6/task10_6/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task10_6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task10_6")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5a0c428e-0e97-4700-bb83-ce3df4510892")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
